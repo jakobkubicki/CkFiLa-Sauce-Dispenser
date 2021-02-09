2de249ea6792ff6a832cf9edb533a14c1aab8f59
jakobkubicki <jakobkub@gmail.com>
2021-02-09 13:22:07 -0800

    Version 0.0.1
    Created base files to code on

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# CkFiLa-Sauce-Dispenser
-The program that runs on the Chik-Fil-A prototype dispenser
+# The Chick-Fil-A Sauce Dispenser Code
+
+This is the code for the Sauce Dispenser prototype
+
+## Installation
+
+N/A
+
+## Usage
+
+N/A
+
+## Contributing
+
+ N/A
+
+## License
+ N/A
---
+++ sauce.cpp
@@ -0,0 +1,16 @@
+/*
+    This program computes the nessesary commands for the sauce dispenser
+    No sources to cite.
+
+    -author Jakob Kubicki
+    -author Ahmad Moltafet
+    -version v0.1 2/9/21
+*/
+
+#include <iostream>
+#include <vector>
+
+int main (){
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
